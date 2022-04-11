25c34b1495ef12131991700a43f820ba49d02829
kmvishnu625 <kmvishnu625@gmail.com>
2022-04-11 10:04:06 +0530

    few tweaks

--- cameraproject/settings.py
+++ cameraproject/settings.py
@@ -79,9 +79,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'camera',
         'USER':'root',
-        'PASSWORD':'welcometodharmadam99',
+        # 'PASSWORD':'welcometodharmadam99',
         'HOST':'localhost',
-        'PORT':'3306',
+        'PORT':'3307',
      
     }
 }
